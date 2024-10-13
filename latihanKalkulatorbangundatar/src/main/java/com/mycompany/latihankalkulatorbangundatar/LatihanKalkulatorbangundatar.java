b0ed96b6439e4769827719e5c526d2e11bc0774c
endig <endig@fuck>
2024-10-14 03:10:19 +0700

    Latihan gui kalkulator sederhana

---
+++ latihanKalkulatorbangundatar/src/main/java/com/mycompany/latihankalkulatorbangundatar/LatihanKalkulatorbangundatar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihankalkulatorbangundatar;
+
+/**
+ *
+ * @author endig
+ */
+public class LatihanKalkulatorbangundatar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
